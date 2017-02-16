9fecc96b613073a8f79dc4a5dff3c654ec1f43a2
BucketHeadWendy <derek.west@outlook.com>
2017-02-15 19:34:15 -0600

    Chapter 4 Updates

--- GreenvilleRevenue/Program.cs
+++ GreenvilleRevenue/Program.cs
@@ -1,15 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//Program written by Dan Plummer and Derek West
+//1.20.17
+//CIS 2620 | Chapter 2 | Case Study #1
 
-namespace GreenvilleRevenue
+using System;
+using static System.Console;
+
+class GreenvilleRevenue
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-        }
+        int lastYearNum, thisYearNum;
+        string lastYear, thisYear;
+        double admission = 25.00, revenue;
+        Write("Enter the number of contestants in last year's competition: ");
+        lastYear = ReadLine();
+        lastYearNum = Int32.Parse(lastYear);
+        Write("Enter the number of contestants in this year's competition: ");
+        thisYear = ReadLine();
+        thisYearNum = Int32.Parse(thisYear);
+        bool moreThisYear = thisYearNum > lastYearNum;
+        revenue = thisYearNum * admission;
+
+        WriteLine();
+        if (thisYearNum > (lastYearNum * 2))
+            WriteLine("The Competition is more than twice as big this year!");
+        else
+            if (thisYearNum > lastYearNum)
+            WriteLine("The competition is bigger than ever!");
+       // WriteLine("There were {0} contestants last year, and {1} contestants this year.", lastYearNum, thisYearNum);
+        WriteLine("To say that there were more contestants this year than last would be {0}", moreThisYear);
+        WriteLine();
+        WriteLine("Expected revenue for this year's competition is: ${0}", revenue.ToString("F2"));
     }
 }
